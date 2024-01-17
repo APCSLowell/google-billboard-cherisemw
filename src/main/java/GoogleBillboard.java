ad18d45da52c9bf56f3672fb1b531d02ba9722f5
cherisemw <86027010+cherisemw@users.noreply.github.com>
2024-01-17 09:55:46 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,15 +1,28 @@
-
-
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+		double answer;
+		for (int i = 2; i < e.length()-10; i++){
+		String digits = e.substring(i, i+10);
+		double num = Double.parseDouble(digits);
+		if (isPrime(num) == true){
+			break;
+		System.out.println(answer);
 	}
 	
-
-	//Finish this function
 	public boolean isPrime(double d){
-	}
-
+		boolean prime = true;
+		if (dNum < 2){
+			prime = false;
+		}
+		for (int i = 2; i <= Math.sqrt(dNum); i++){
+			if (dNum % i == 0){
+				prime = false;
+			}  
+		}
+		if (prime == true){
+			answer = dNum;
+		}
+			return prime;
+		}
 }
-
